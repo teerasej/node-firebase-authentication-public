2e434fc7b3e1eeea8b932f7c7fe4bba65245c8d9
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-12-04 20:25:08 +0700

    apply action code with redirect url after confirmed

--- index.js
+++ index.js
@@ -3,29 +3,38 @@ import * as readline from "readline-sync";
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAPov2IbRYu06oi7fNAGUZ_bJIo8lAseWo",
-    authDomain: "ionicpwa-d8c79.firebaseapp.com",
-    databaseURL: "https://ionicpwa-d8c79.firebaseio.com",
-    projectId: "ionicpwa-d8c79",
-    storageBucket: "ionicpwa-d8c79.appspot.com",
-    messagingSenderId: "236296839382",
-    appId: "1:236296839382:web:935031e7f83afdf3f8e977"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 (async () => {
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyAPov2IbRYu06oi7fNAGUZ_bJIo8lAseWo",
+        authDomain: "ionicpwa-d8c79.firebaseapp.com",
+        databaseURL: "https://ionicpwa-d8c79.firebaseio.com",
+        projectId: "ionicpwa-d8c79",
+        storageBucket: "ionicpwa-d8c79.appspot.com",
+        messagingSenderId: "236296839382",
+        appId: "1:236296839382:web:935031e7f83afdf3f8e977"
+    };
+    
+    // Initialize Firebase
+    console.log('...initializing app')
+    const app = initializeApp(firebaseConfig);
+    console.log('...initializing app success')
+
     try {
-        while (true) {
+
+        let command = -1;
+
+        do {
             console.log('Firebase Client')
 
             console.log('   0: Exit')
             console.log('   1: Create new user')
             console.log('   2: Sign in user')
 
-            const command = readline.questionInt('Select command:')
+            console.log('Type command:')
+            const command = readline.questionInt()
 
             if (command == 0) {
                 break;
@@ -43,18 +52,25 @@ const app = initializeApp(firebaseConfig);
                         const userCredential = await createUserWithEmailAndPassword(auth, email, password)
                         const user = userCredential.user
                         console.log(user)
-
-                        await sendEmailVerification(user)
+                        
+                        let actionCodeSettings = {
+                            url: 'http://localhost:3000/?email=' + user.email,
+                        }
+                        await sendEmailVerification(user,actionCodeSettings)
                     } catch (e) {
-                        console.log('error:',e)
+                        if(e.code == 'auth/email-already-in-use') {
+                            console.error('Email already used.')
+                        } else {
+                            console.error('error:',e)
+                        }
                     }
                     break
 
                 case 2:
-
+                    
                     break
             }
-        }
+        } while (command != 0) 
     } catch (e) {
         console.log('error:',e)
     }
