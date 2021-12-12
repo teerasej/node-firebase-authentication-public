e8325a148aee394ee158ee4daed9ff2b13ad0d1b
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-12-12 22:15:27 +0700

    adjust menu and switch to use callable function

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 import * as readline from "readline-sync";
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword, fetchSignInMethodsForEmail } from "firebase/auth";
-import { getFirestore, collection, addDoc, getDocs, setDoc, doc, serverTimestamp, query, where, deleteDoc, getDoc, updateDoc } from 'firebase/firestore';
-import { getStorage, ref, uploadBytes, listAll, deleteObject, getDownloadURL } from "firebase/storage";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getFunctions, connectFunctionsEmulator, httpsCallable } from "firebase/functions";
+
 import * as fs from 'fs/promises'
 import Axios from "axios";
 
@@ -63,24 +63,10 @@ const signIn = async (email, password) => {
             console.log('\n==== Firebase Client ====')
 
             console.log('   0: Exit')
-            console.log('   1: Create new user')
-            console.log('   2: Sign in user')
-            console.log('   3: Check email exists')
+            console.log('   1: Sign in user')
 
             console.log('\n --- Note ---')
-            console.log('   4: Create tweet')
-            console.log('   5: List tweets')
-            console.log('   6: Remove tweet')
-            console.log('   7: Edit tweet')
-
-            console.log('\n --- File ---')
-            console.log('   8: Upload File')
-            console.log('   9: List Files')
-            console.log('   10: Delete File')
-            console.log('   11: Download File')
-
-            console.log('\n --- Management ---')
-            console.log('   12: Reset Password')
+            console.log('   2: Create tweet')
 
             console.log('Type command:')
             const command = readline.questionInt()
@@ -91,33 +77,8 @@ const signIn = async (email, password) => {
 
             switch (command) {
 
+               
                 case 1:
-                    try {
-                        const email = readline.questionEMail()
-                        const password = readline.question('password:')
-                        console.log(email, password)
-
-                        const auth = getAuth()
-                        const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-                        const user = userCredential.user
-                        console.log(user)
-
-                        signedInUser = user;
-
-                        let actionCodeSettings = {
-                            url: 'http://localhost:3000/?email=' + user.email,
-                        }
-                        await sendEmailVerification(user, actionCodeSettings)
-                    } catch (e) {
-                        if (e.code == 'auth/email-already-in-use') {
-                            console.error('Email already used.')
-                        } else {
-                            console.error('error:', e)
-                        }
-                    }
-                    break
-
-                case 2:
                     const email = readline.questionEMail()
                     const password = readline.question('password:')
                     signedInUser = await signIn(email, password)
@@ -125,26 +86,8 @@ const signIn = async (email, password) => {
 
                     break
 
-                case 3:
+                case 2:
 
-
-                    try {
-                        const email = readline.questionEMail()
-                        const auth = getAuth()
-                        const methods = await fetchSignInMethodsForEmail(auth, email)
-
-                        if (methods.length > 0) {
-                            console.log(`${email} already used.`)
-                        } else {
-                            console.log(`${email} is available`)
-                        }
-
-                    } catch (e) {
-                        console.error('error:', e)
-                    }
-                    break
-
-                case 4:
                     try {
                         if (!signedInUser) {
                             console.log('   please sign in first.')
@@ -157,291 +100,25 @@ const signIn = async (email, password) => {
                             continue
                         }
 
-                        console.log('Attach photo?')
-                        console.log('1. No')
-                        console.log('2. Yes')
-                        const attachPhotoChoice = readline.questionInt('Type command:')
+                        const functions = getFunctions(app);
+                        // Remove this if production
+                        connectFunctionsEmulator(functions, "localhost", 5001);
 
-                        let requestAttachPhoto = false
+                        const createMessage = httpsCallable(functions, 'createMessage');
 
-                        if (attachPhotoChoice != 1) {
-                            requestAttachPhoto = true
-                        }
+                        const result = await createMessage({ userId: signedInUser.uid, message: message})
 
-                        const fireStore = getFirestore(app)
-                        const noteCollection = collection(fireStore, '/notes')
-                        const doc = await addDoc(noteCollection, {
-                            userId: signedInUser.uid,
-                            message: message,
-                            createdDate: serverTimestamp()
-                        })
+                        console.log(`   message created ${result}`)
+                        
 
-                        if (requestAttachPhoto) {
-
-                            const fileName = readline.question('File name to upload:')
-
-                            if (!fileName || fileName.length == 0) {
-                                continue
-                            }
-
-                            const storage = getStorage(app)
-                            const remoteFileRef = ref(storage, `images/${fileName}`)
-                            const buffer = await fs.readFile(fileName)
-
-                            console.log('   uploading...')
-                            const result = await uploadBytes(remoteFileRef, buffer)
-                            console.log('   uploaded...')
-
-                            await setDoc(doc, {
-                                attachFile: result.ref.fullPath
-                            }, { merge: true })
-                        }
+                        
                     } catch (error) {
                         console.log('Error', error)
                     }
 
                     break
 
-                case 5:
-
-                    try {
-                        if (!signedInUser) {
-                            console.log('   please sign in first.')
-                            continue
-                        }
-
-                        const fireStore = getFirestore(app)
-                        const noteCollection = collection(fireStore, '/notes')
-
-                        const q = query(noteCollection, where("userId", "==", signedInUser.uid));
-
-                        const messageSnapshots = await getDocs(q)
-                        const messageArray = messageSnapshots.docs.map(doc => {
-                            return { ...doc.data(), id: doc.id }
-                        });
-
-
-
-
-                        for (let index = 0; index < messageArray.length; index++) {
-                            const message = messageArray[index];
-                            console.log(`[${index}]: ${message.id}`)
-                            console.log(JSON.stringify(message, null, '\t'))
-                        }
-                    } catch (error) {
-                        console.log('Error', error)
-                    }
-
-
-                    break
-
-                case 6:
-
-                    try {
-                        if (!signedInUser) {
-                            console.log('   please sign in first.')
-                            continue
-                        }
-
-                        const messageId = readline.question('Message ID:')
-
-                        const fireStore = getFirestore(app)
-                        const deletingDoc = doc(fireStore, '/notes', messageId)
-                        await deleteDoc(deletingDoc)
-
-                        console.log(`   doc ${messageId} deleted.`)
-
-                    } catch (error) {
-                        console.log('Error', error)
-                    }
-
-                    break
-
-                case 7:
-
-                    try {
-                        if (!signedInUser) {
-                            console.log('   please sign in first.')
-                            continue
-                        }
-
-                        const messageId = readline.question('Message ID to Edit:')
-
-
-                        const fireStore = getFirestore(app)
-                        const targetDoc = doc(fireStore, '/notes', messageId)
-                        const editingDoc = await getDoc(targetDoc)
-
-                        console.log(`Current message: ${editingDoc.data().message}`)
-                        const newMessage = readline.question('New Message:')
-
-                        updateDoc(targetDoc, {
-                            message: newMessage,
-                            updatedDate: serverTimestamp()
-                        })
-
-                        console.log(`   doc ${messageId} updated with '${newMessage}'.`)
-
-                    } catch (error) {
-                        console.log('Error', error)
-                    }
-
-                    break
-
-                case 8:
-                    try {
-                        if (!signedInUser) {
-                            console.log('   please sign in first.')
-                            continue
-                        }
-
-                        const fileName = readline.question('File name to upload:')
-
-                        if (!fileName || fileName.length == 0) {
-                            continue
-                        }
-
-                        const remoteLocation = readline.question('Remote location (ex. asset/images):')
-
-                        if (!remoteLocation || remoteLocation.length == 0) {
-                            continue
-                        }
-
-                        const fireStore = getFirestore(app)
-                        const storage = getStorage(app)
-                        const remoteFileRef = ref(storage, `${remoteLocation}/${fileName}`)
-                        const buffer = await fs.readFile(fileName)
-
-                        console.log(`   uploading '${fileName}' to '${remoteLocation}'...`)
-                        const result = await uploadBytes(remoteFileRef, buffer)
-                        console.log('   uploaded...')
-
-                    } catch (error) {
-                        console.log('Error', error)
-                    }
-
-
-                    break
-
-                case 9:
-
-                    try {
-                        if (!signedInUser) {
-                            console.log('   please sign in first.')
-                            continue
-                        }
-
-                        const fireStore = getFirestore(app)
-                        const storage = getStorage(app)
-
-                        const rootRef = ref(storage)
-                        // const rootRef = ref(storage, 'path')
-
-                        const fileList = await listAll(rootRef)
-
-                        if (fileList.prefixes.length > 0) {
-                            console.log('Folders:')
-                            fileList.prefixes.forEach(folder => {
-                                console.log(`   ${folder.name}`)
-                            });
-                        }
-
-                        if (fileList.items.length > 0) {
-                            console.log('\nFiles:')
-                            fileList.items.forEach(file => {
-                                console.log(`   ${file.name}`)
-                            });
-                        }
-
-
-
-                    } catch (error) {
-                        console.log('Error:', error)
-                    }
-
-                    break
-
-                case 10:
-
-                    try {
-                        if (!signedInUser) {
-                            console.log('   please sign in first.')
-                            continue
-                        }
-
-                        const targetFileName = readline.question('Target remote file:')
-
-                        if (!targetFileName || targetFileName.length == 0) {
-                            continue
-                        }
-
-                        const fireStore = getFirestore(app)
-                        const storage = getStorage(app)
-                        const fileRef = ref(storage, targetFileName)
-
-                        console.log(`   deleting file: '${targetFileName}'.`)
-                        await deleteObject(fileRef)
-                        console.log(`   deleted.`)
-
-                    } catch (error) {
-                        console.log('Error:', error)
-                    }
-
-                    break
-
-                case 11:
-
-                    try {
-                        if (!signedInUser) {
-                            console.log('   please sign in first.')
-                            continue
-                        }
-
-                        const targetFileName = readline.question('Target remote file:')
-
-                        if (!targetFileName || targetFileName.length == 0) {
-                            continue
-                        }
-
-                        const fireStore = getFirestore(app)
-                        const storage = getStorage(app)
-                        const fileRef = ref(storage, targetFileName)
-
-                        console.log(`   Downloading file: '${targetFileName}'.`)
-                        const downloadURL = await getDownloadURL(fileRef)
-
-                        const res = await Axios.get(downloadURL, { responseType: "arraybuffer" });
-                        await fs.writeFile(fileRef.name, res.data);
-
-                        console.log(`   downloaded.`)
-
-                    } catch (error) {
-                        console.log('Error:', error)
-                    }
-
-                    break
-
-                case 12:
-
-                    try {
-                       
-                        const email = readline.question('Email:')
-
-                        if (!email || email.length == 0) {
-                            continue
-                        }
-
-                        const auth = getAuth()
-                        auth.languageCode = 'th'
-                        await sendPasswordResetEmail(auth, email)
-
-                        console.log(`   Password reset email sent.`)
-
-                    } catch (error) {
-                        console.log('Error:', error)
-                    }
-
-                    break
+                
             }
         } while (command != 0)
     } catch (e) {
